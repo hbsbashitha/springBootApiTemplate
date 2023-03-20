6f8d1ecdd5c70415f192131fb2f038c7dddcf239
Bhashitha Ranasinghe <Bhashitha@ebuilder.local>
2023-03-20 09:32:07 +0530

    init

---
+++ src/main/java/com/bhashitha/springBootApiTemplate/SpringBootApiTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.bhashitha.springBootApiTemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApiTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApiTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bhashitha/springBootApiTemplate/SpringBootApiTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhashitha.springBootApiTemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApiTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
